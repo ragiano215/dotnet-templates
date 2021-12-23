8ad8b743b4e8f1da89a54cee81ebdfe4ef4f6672
Rambert Yan <ragiano215@gmail.com>
2021-12-22 22:43:14 -0500

    Updated `Program.fs` with new template pattern.

---
+++ equinox-fc/Web/Infrastructure.fs
@@ -0,0 +1,55 @@
+[<AutoOpen>]
+module Fc.Web.Infrastructure
+
+open System
+
+open Serilog
+
+module EnvVar =
+
+    let tryGet varName : string option = Environment.GetEnvironmentVariable varName |> Option.ofObj
+
+type Equinox.CosmosStore.CosmosStoreConnector with
+
+    member private x.LogConfiguration(connectionName, databaseId, containerId) =
+        let o = x.Options
+        let timeout, retries429, timeout429 = o.RequestTimeout, o.MaxRetryAttemptsOnRateLimitedRequests, o.MaxRetryWaitTimeOnRateLimitedRequests
+        Log.Information("CosmosDb {name} {mode} {endpointUri} timeout {timeout}s; Throttling retries {retries}, max wait {maxRetryWaitTime}s",
+                        connectionName, o.ConnectionMode, x.Endpoint, timeout.TotalSeconds, retries429, let t = timeout429.Value in t.TotalSeconds)
+        Log.Information("CosmosDb {name} Database {database} Container {container}",
+                        connectionName, databaseId, containerId)
+
+    /// Use sparingly; in general one wants to use CreateAndInitialize to avoid slow first requests
+    member x.CreateUninitialized(databaseId, containerId) =
+        x.CreateUninitialized().GetDatabase(databaseId).GetContainer(containerId)
+
+    /// Creates a CosmosClient suitable for running a CFP via CosmosStoreSource
+    member private x.ConnectMonitored(databaseId, containerId, ?connectionName) =
+        x.LogConfiguration(defaultArg connectionName "Source", databaseId, containerId)
+        x.CreateUninitialized(databaseId, containerId)
+
+    /// Connects to a Store as both a ChangeFeedProcessor Monitored Container and a CosmosStoreClient
+    member x.ConnectStoreAndMonitored(databaseId, containerId) =
+        let monitored = x.ConnectMonitored(databaseId, containerId, "Main")
+        let storeClient = Equinox.CosmosStore.CosmosStoreClient(monitored.Database.Client, databaseId, containerId)
+        storeClient, monitored
+
+module CosmosStoreContext =
+
+    /// Create with default packing and querying policies. Search for other `module CosmosStoreContext` impls for custom variations
+    let create (storeClient : Equinox.CosmosStore.CosmosStoreClient) =
+        let maxEvents = 256
+        Equinox.CosmosStore.CosmosStoreContext(storeClient, tipMaxEvents=maxEvents)
+
+[<System.Runtime.CompilerServices.Extension>]
+type Logging() =
+
+    [<System.Runtime.CompilerServices.Extension>]
+    static member Configure(configuration : LoggerConfiguration, ?verbose) =
+        configuration
+            .Destructure.FSharpTypes()
+            .Enrich.FromLogContext()
+            .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
+        |> fun c -> if verbose = Some true then c.MinimumLevel.Debug() else c
+        |> fun c -> let t = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {NewLine}{Exception}"
+                    c.WriteTo.Console(theme=Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code, outputTemplate=t)
--- equinox-fc/Web/Program.fs
+++ equinox-fc/Web/Program.fs
@@ -9,35 +9,29 @@ open Microsoft.Extensions.Hosting
 open Serilog
 open System
 
-module EnvVar =
+exception MissingArg of message : string with override this.Message = this.message
 
-    let tryGet varName : string option = Environment.GetEnvironmentVariable varName |> Option.ofObj
-    let set varName value : unit = Environment.SetEnvironmentVariable(varName, value)
+type Configuration(tryGet) =
 
-module Configuration =
+    let get key =
+        match tryGet key with
+        | Some value -> value
+        | None -> raise (MissingArg (sprintf "Missing Argument/Environment Variable %s" key))
+    let isTrue varName = tryGet varName |> Option.exists (fun s -> String.Equals(s, bool.TrueString, StringComparison.OrdinalIgnoreCase))
 
-    let private initEnvVar var key loadF =
-        if None = EnvVar.tryGet var then
-            printfn "Setting %s from %A" var key
-            EnvVar.set var (loadF key)
+    member _.EventStoreHost =               get "EQUINOX_ES_HOST"
+    member _.EventStoreTcp =                isTrue "EQUINOX_ES_TCP"
+    member _.EventStorePort =               tryGet "EQUINOX_ES_PORT" |> Option.map int
+    member _.EventStoreUsername =           get "EQUINOX_ES_USERNAME"
+    member _.EventStorePassword =           get "EQUINOX_ES_PASSWORD"
 
-    let initialize () =
-        // e.g. initEnvVar     "EQUINOX_COSMOS_CONTAINER"    "CONSUL KEY" readFromConsul
-        () // TODO add any custom logic preprocessing commandline arguments and/or gathering custom defaults from external sources, etc
+    member _.CosmosConnection =             get "EQUINOX_COSMOS_CONNECTION"
+    member _.CosmosDatabase =               get "EQUINOX_COSMOS_DATABASE"
+    member _.CosmosContainer =              get "EQUINOX_COSMOS_CONTAINER"
 
 module Args =
 
-    exception MissingArg of string
-    let private getEnvVarForArgumentOrThrow varName argName =
-        match EnvVar.tryGet varName with
-        | None -> raise (MissingArg(sprintf "Please provide a %s, either as an argument or via the %s environment variable" argName varName))
-        | Some x -> x
-    let private defaultWithEnvVar varName argName = function
-        | None -> getEnvVarForArgumentOrThrow varName argName
-        | Some x -> x
-    let isEnvVarTrue varName = EnvVar.tryGet varName |> Option.exists (fun s -> String.Equals(s, bool.TrueString, StringComparison.OrdinalIgnoreCase))
     open Argu
-    open Equinox.Cosmos
     open Equinox.EventStore
     [<NoEquality; NoComparison>]
     type Parameters =
@@ -51,14 +45,14 @@ module Args =
                 | Verbose ->                "request Verbose Logging. Default: off."
                 | Es _ ->                   "specify EventStore input parameters."
                 | Cosmos _ ->               "specify CosmosDB input parameters."
-    and Arguments(a : ParseResults<Parameters>) =
+    and Arguments(c : Configuration, a : ParseResults<Parameters>) =
         member __.Verbose =                 a.Contains Parameters.Verbose
         member __.StatsInterval =           TimeSpan.FromMinutes 1.
 
         member val Source : Choice<EsArguments, CosmosArguments> =
             match a.TryGetSubCommand() with
-            | Some (Es es) -> Choice1Of2 (EsArguments es)
-            | Some (Cosmos cosmos) -> Choice2Of2 (CosmosArguments cosmos)
+            | Some (Es es) -> Choice1Of2 (EsArguments (c, es))
+            | Some (Cosmos cosmos) -> Choice2Of2 (CosmosArguments (c, cosmos))
             | _ -> raise (MissingArg "Must specify one of cosmos or es for Src")
     and [<NoEquality; NoComparison>] EsParameters =
         | [<AltCommandLine "-V">]           Verbose
@@ -81,18 +75,18 @@ module Args =
                 | Timeout _ ->              "specify operation timeout in seconds. Default: 20."
                 | Retries _ ->              "specify operation retries. Default: 3."
                 | HeartbeatTimeout _ ->     "specify heartbeat timeout in seconds. Default: 1.5."
-    and EsArguments(a : ParseResults<EsParameters>) =
+    and EsArguments(c : Configuration, a : ParseResults<EsParameters>) =
         member __.Discovery =
             match __.Tcp, __.Port with
             | false, None ->   Discovery.GossipDns            __.Host
             | false, Some p -> Discovery.GossipDnsCustomPort (__.Host, p)
             | true, None ->    Discovery.Uri                 (UriBuilder("tcp", __.Host, 1113).Uri)
             | true, Some p ->  Discovery.Uri                 (UriBuilder("tcp", __.Host, p).Uri)
-        member __.Tcp =                     a.Contains Tcp || isEnvVarTrue "EQUINOX_ES_TCP"
-        member __.Port =                    match a.TryGetResult Port with Some x -> Some x | None -> EnvVar.tryGet "EQUINOX_ES_PORT" |> Option.map int
-        member __.Host =                    a.TryGetResult Host     |> defaultWithEnvVar "EQUINOX_ES_HOST"     "Host"
-        member __.User =                    a.TryGetResult Username |> defaultWithEnvVar "EQUINOX_ES_USERNAME" "Username"
-        member __.Password =                a.TryGetResult Password |> defaultWithEnvVar "EQUINOX_ES_PASSWORD" "Password"
+        member __.Tcp =                     a.Contains Tcp || c.EventStoreTcp
+        member __.Port =                    a.TryGetResult Port     |> Option.orElse c.EventStorePort
+        member __.Host =                    a.TryGetResult Host     |> Option.defaultValue c.EventStoreHost
+        member __.User =                    a.TryGetResult Username |> Option.defaultValue c.EventStoreUsername
+        member __.Password =                a.TryGetResult Password |> Option.defaultValue c.EventStorePassword
         member __.Retries =                 a.GetResult(EsParameters.Retries, 3)
         member __.Timeout =                 a.GetResult(EsParameters.Timeout, 20.) |> TimeSpan.FromSeconds
         member __.Heartbeat =               a.GetResult(HeartbeatTimeout, 1.5) |> TimeSpan.FromSeconds
@@ -107,58 +101,59 @@ module Args =
             Connector(x.User, x.Password, x.Timeout, x.Retries, log=log, heartbeatTimeout=x.Heartbeat, tags=tags)
                 .Establish(appName, discovery, connectionStrategy) |> Async.RunSynchronously
     and [<NoEquality; NoComparison>] CosmosParameters =
+        | [<AltCommandLine "-Z"; Unique>]   FromTail
+        | [<AltCommandLine "-mi"; Unique>]  MaxItems of int
+        | [<AltCommandLine "-l"; Unique>]   LagFreqM of float
+        | [<AltCommandLine "-a"; Unique>]   LeaseContainer of string
+
+        | [<AltCommandLine "-m">]           ConnectionMode of Microsoft.Azure.Cosmos.ConnectionMode
         | [<AltCommandLine "-s">]           Connection of string
-        | [<AltCommandLine "-m">]           ConnectionMode of ConnectionMode
         | [<AltCommandLine "-d">]           Database of string
-        | [<AltCommandLine "-c">]           Container of string
+        | [<AltCommandLine "-c"; Unique>]   Container of string // Actually Mandatory, but stating that is not supported
         | [<AltCommandLine "-o">]           Timeout of float
         | [<AltCommandLine "-r">]           Retries of int
         | [<AltCommandLine "-rt">]          RetriesWaitTime of float
         interface IArgParserTemplate with
-            member a.Usage =
-                match a with
+            member a.Usage = a |> function
+                | FromTail ->               "(iff the Consumer Name is fresh) - force skip to present Position. Default: Never skip an event."
+                | MaxItems _ ->             "maximum item count to request from feed. Default: unlimited"
+                | LagFreqM _ ->             "frequency (in minutes) to dump lag stats. Default: 1"
+                | LeaseContainer _ ->       "specify Container Name for Leases container. Default: `sourceContainer` + `-aux`."
+
                 | ConnectionMode _ ->       "override the connection mode. Default: Direct."
                 | Connection _ ->           "specify a connection string for a Cosmos account. (optional if environment variable EQUINOX_COSMOS_CONNECTION specified)"
-                | Database _ ->             "specify a database name for Cosmos store. (optional if environment variable EQUINOX_COSMOS_DATABASE specified)"
-                | Container _ ->            "specify a container name for Cosmos store. (optional if environment variable EQUINOX_COSMOS_CONTAINER specified)"
+                | Database _ ->             "specify a database name for Cosmos account. (optional if environment variable EQUINOX_COSMOS_DATABASE specified)"
+                | Container _ ->            "specify a container name within `Database`"
                 | Timeout _ ->              "specify operation timeout in seconds. Default: 5."
                 | Retries _ ->              "specify operation retries. Default: 1."
                 | RetriesWaitTime _ ->      "specify max wait-time for retry when being throttled by Cosmos in seconds. Default: 5."
-    and CosmosArguments(a : ParseResults<CosmosParameters>) =
-        member __.Mode =                    a.GetResult(CosmosParameters.ConnectionMode, Equinox.Cosmos.ConnectionMode.Direct)
-        member __.Connection =              a.TryGetResult CosmosParameters.Connection |> defaultWithEnvVar "EQUINOX_COSMOS_CONNECTION" "Connection"
-        member __.Database =                a.TryGetResult CosmosParameters.Database   |> defaultWithEnvVar "EQUINOX_COSMOS_DATABASE"   "Database"
-        member __.Container =               a.TryGetResult CosmosParameters.Container  |> defaultWithEnvVar "EQUINOX_COSMOS_CONTAINER"  "Container"
-        member __.Timeout =                 a.GetResult(CosmosParameters.Timeout, 5.) |> TimeSpan.FromSeconds
-        member __.Retries =                 a.GetResult(CosmosParameters.Retries, 1)
-        member __.MaxRetryWaitTime =        a.GetResult(CosmosParameters.RetriesWaitTime, 5.) |> TimeSpan.FromSeconds
-        member x.BuildConnectionDetails() =
-            let (Discovery.UriAndKey (endpointUri, _) as discovery) = Discovery.FromConnectionString x.Connection
-            Log.Information("CosmosDb {mode} {endpointUri} Database {database} Container {container}",
-                x.Mode, endpointUri, x.Database, x.Container)
-            Log.Information("CosmosDb timeout {timeout}s; Throttling retries {retries}, max wait {maxRetryWaitTime}s",
-                (let t = x.Timeout in t.TotalSeconds), x.Retries, (let t = x.MaxRetryWaitTime in t.TotalSeconds))
-            let connector = Equinox.Cosmos.Connector(x.Timeout, x.Retries, x.MaxRetryWaitTime, Log.Logger, mode=x.Mode)
-            discovery, x.Database, x.Container, connector
+    and CosmosArguments(c : Configuration, a : ParseResults<CosmosParameters>) =
+        let discovery =                     a.TryGetResult Connection |> Option.defaultWith (fun () -> c.CosmosConnection) |> Equinox.CosmosStore.Discovery.ConnectionString
+        let mode =                          a.TryGetResult ConnectionMode
+        let timeout =                       a.GetResult(Timeout, 5.) |> TimeSpan.FromSeconds
+        let retries =                       a.GetResult(Retries, 1)
+        let maxRetryWaitTime =              a.GetResult(RetriesWaitTime, 5.) |> TimeSpan.FromSeconds
+        let connector =                     Equinox.CosmosStore.CosmosStoreConnector(discovery, timeout, retries, maxRetryWaitTime, ?mode = mode)
+        let database =                      a.TryGetResult Database |> Option.defaultWith (fun () -> c.CosmosDatabase)
+        let containerId =                   a.GetResult Container
+        let leaseContainerId =              a.GetResult(LeaseContainer, containerId + "-aux")
+        let fromTail =                      a.Contains FromTail
+        let maxItems =                      a.TryGetResult MaxItems
+        let lagFrequency =                  a.GetResult(LagFreqM, 1.) |> TimeSpan.FromMinutes
+        member private _.ConnectLeases() =  connector.CreateUninitialized(database, leaseContainerId)
+        member x.MonitoringParams() =
+            let leases : Microsoft.Azure.Cosmos.Container = x.ConnectLeases()
+            Log.Information("ChangeFeed Leases Database {db} Container {container}. MaxItems limited to {maxItems}",
+                leases.Database.Id, leases.Id, Option.toNullable maxItems)
+            if fromTail then Log.Warning("(If new projector group) Skipping projection of all existing events.")
+            (leases, fromTail, maxItems, lagFrequency)
+        member _.ConnectStoreAndMonitored() = connector.ConnectStoreAndMonitored(database, containerId)
 
     /// Parse the commandline; can throw exceptions in response to missing arguments and/or `-h`/`--help` args
-    let parse argv =
+    let parse tryGetConfigValue argv : Arguments =
         let programName = System.Reflection.Assembly.GetEntryAssembly().GetName().Name
-        let parser = ArgumentParser.Create<Parameters>(programName = programName)
-        parser.ParseCommandLine argv |> Arguments
-
-module Logging =
-
-    let initialize verbose =
-        Log.Logger <-
-            LoggerConfiguration()
-                .Destructure.FSharpTypes()
-                .Enrich.FromLogContext()
-                .MinimumLevel.Override("Microsoft.AspNetCore", Serilog.Events.LogEventLevel.Warning)
-            |> fun c -> if verbose then c.MinimumLevel.Debug() else c
-            |> fun c -> let t = "[{Timestamp:HH:mm:ss} {Level:u3}] {partitionKeyRangeId,2} {Message:lj} {NewLine}{Exception}"
-                        c.WriteTo.Console(theme=Sinks.SystemConsole.Themes.AnsiConsoleTheme.Code, outputTemplate=t)
-            |> fun c -> c.CreateLogger()
+        let parser = ArgumentParser.Create<Parameters>(programName=programName)
+        Arguments(Configuration tryGetConfigValue, parser.ParseCommandLine argv)
 
 let [<Literal>] AppName = "Fc.Web"
 
@@ -184,21 +179,22 @@ type Startup() =
             .UseEndpoints(fun endpoints -> endpoints.MapControllers() |> ignore)
             |> ignore
 
+open Fc.Domain
+
 let build (args : Args.Arguments) =
     let cache = Equinox.Cache(AppName, sizeMb=10)
-    let create =
+    let store =
         match args.Source with
         | Choice1Of2 es ->
             let connection = es.Connect(Log.Logger, Log.Logger, AppName, Equinox.EventStore.ConnectionStrategy.ClusterSingle Equinox.EventStore.NodePreference.Master)
-            let context = Equinox.EventStore.Context(connection, Equinox.EventStore.BatchingPolicy(maxBatchSize=500))
-            Fc.Domain.StockProcessManager.EventStore.create (context, cache)
+            let context = Equinox.EventStore.EventStoreContext(connection, Equinox.EventStore.BatchingPolicy(maxBatchSize=500))
+            Config.Store.Esdb (context, cache)
         | Choice2Of2 cosmos ->
-            let (discovery, database, container, connector) = cosmos.BuildConnectionDetails()
-            let connection = connector.Connect(AppName, discovery) |> Async.RunSynchronously
-            let context = Equinox.Cosmos.Context(connection, database, container)
-            Fc.Domain.StockProcessManager.Cosmos.create (context, cache)
+            let (client, _monitored) = cosmos.ConnectStoreAndMonitored()
+            let context = CosmosStoreContext.create(client)
+            Config.Store.Cosmos (context, cache)
     let inventoryId = InventoryId.parse "FC000"
-    create inventoryId (1000, 10) (1000, 5, 3)
+    StockProcessManager.Config.create inventoryId (1000, 10) (1000, 5, 3) store
 
 let run argv args =
     let processManager = build args
@@ -212,13 +208,11 @@ let run argv args =
 
 [<EntryPoint>]
 let main argv =
-    try let args = Args.parse argv
-        try Logging.initialize args.Verbose
-            try Configuration.initialize ()
-                run argv args
-                0
-            with e when not (e :? Args.MissingArg) -> Log.Fatal(e, "Exiting"); 2
+    try let args = Args.parse EnvVar.tryGet argv
+        try Log.Logger <- LoggerConfiguration().Configure(verbose=args.Verbose).CreateLogger()
+            try run argv args; 0
+            with e when not (e :? MissingArg) -> Log.Fatal(e, "Exiting"); 2
         finally Log.CloseAndFlush()
-    with Args.MissingArg msg -> eprintfn "%s" msg; 1
+    with MissingArg msg -> eprintfn "%s" msg; 1
         | :? Argu.ArguParseException as e -> eprintfn "%s" e.Message; 1
         | e -> eprintf "Exception %s" e.Message; 1
